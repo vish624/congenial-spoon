0d7c02ee22e639b37e2dc45332b33c0ecfea36bb
Vishaan vivek <Vishaan.vivek@student.education.wa.edu.au>
2025-07-25 18:15:54 +0800

    dear diary, I have made the code for the grid setup for the game for the blocks to move through.

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ WIDTH, HEIGHT = 400, 60
 WIN = pygame.display.set_mode(WIDTH,HEIGHT)
 pygame.display.set_caption("Crazy Tetris")
 clock = pygame.time.clock()
+
+#Grid setup
+ROWS = 20
+COLS = 10
+BLOCK_SIZE = WIDTH // COLS
+grid = [[(0,0,0) for _ in range(COLS)] for _ in range(ROWS)]
+
+# Shapes for the tetris game
